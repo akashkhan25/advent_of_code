f7d781a4768d2808c21971f079f0bf73a7ea81f2
Akash Khan <akash.khan@aktsk.com>
2021-12-01 18:34:59 +0800

    feat: Add day1 solution

---
+++ lib/aoc/year2021/day1.ex
@@ -0,0 +1,28 @@
+defmodule AOC.Year2021.Day1 do
+  use AOC, day: 1
+
+  def part1(list, count \\ 0)
+
+  def part1([a, b | tail] = _list, count) do
+    count = if b > a, do: count + 1, else: count
+    part1([b | tail], count)
+  end
+
+  def part1(_, count), do: count
+
+  def part2(list, count \\ 0)
+
+  def part2([a, b, c, d | tail] = _list, count) do
+    count = if a < d, do: count + 1, else: count
+    part2([b, c, d | tail], count)
+  end
+
+  def part2(_, count), do: count
+
+  def parse_input() do
+    input()
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+  end
+end
