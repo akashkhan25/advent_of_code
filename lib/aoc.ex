3f83f5624c5bde4a425e76dcdee0892ad69d0692
Akash Khan <akash.khan@aktsk.com>
2021-12-01 18:30:11 +0800

    feat: Init project

---
+++ lib/aoc.ex
@@ -0,0 +1,16 @@
+defmodule AOC do
+  @moduledoc """
+  Documentation for `AOC`.
+  """
+
+  @doc """
+  Hello world.
+  ## Examples
+      iex> AOC.hello()
+      :world
+  """
+  def hello do
+    :world
+  end
+end
+
