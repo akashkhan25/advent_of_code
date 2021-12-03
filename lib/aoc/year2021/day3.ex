1f27456013270db82742cf6c7e943810b803b2e5
Akash Khan <akash.khan@aktsk.com>
2021-12-03 22:48:24 +0800

    refactor: stop using input bitsize as param

--- lib/aoc/year2021/day3.ex
+++ lib/aoc/year2021/day3.ex
@@ -1,20 +1,20 @@
 defmodule AOC.Year2021.Day3 do
   use AOC, day: 3
 
-  def part1(input \\ parse_input(), size \\ 11) do
+  def part1(input \\ parse_input()) do
     input
-    |> power_consumption(size)
+    |> power_consumption()
     |> calc_ans()
   end
 
-  def part2(input \\ parse_input(), size \\ 11) do
-    {calc_oxy(input, size, "", 0), calc_co2(input, size, 0)}
+  def part2(input \\ parse_input()) do
+    {calc_oxy(input, "", 0), calc_co2(input, 0)}
     |> calc_ans()
   end
 
-  def power_consumption(input, size) do
+  def power_consumption(input) do
     input
-    |> find_frequencies(size)
+    |> find_frequencies()
     |> Enum.map(&find_rates/1)
     |> Enum.reduce({"", ""}, fn {a, b}, {gam, eps} -> {gam <> a, eps <> b} end)
   end
@@ -25,10 +25,11 @@ defmodule AOC.Year2021.Day3 do
     |> Enum.map(&String.graphemes/1)
   end
 
-  def find_frequencies(input, size) do
-    for i <- 0..size do
-      Enum.frequencies_by(input, &Enum.at(&1, i))
-    end
+  def find_frequencies(input) do
+    input
+    |> Stream.zip
+    |> Stream.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.frequencies/1)
   end
 
   defp calc_ans(val) do
@@ -55,30 +56,28 @@ defmodule AOC.Year2021.Day3 do
   defp find_smaller(%{"0" => _val}), do: "0"
   defp find_smaller(%{"1" => _val}), do: "1"
 
-  def split_by_bit(input, index, val) do
-    input
-    |> Enum.split_with(&(Enum.at(&1, index) == val))
-  end
+  def calc_oxy([_], val, _index), do: val
 
-  def calc_oxy([_], _size, val, _index), do: val
-
-  def calc_oxy(input, size, val, index) do
-    check_val = input |> find_frequencies(size) |> Enum.at(index) |> find_larger()
+  def calc_oxy(input, val, index) do
+    check_val = input 
+    |> find_frequencies() 
+    |> Enum.at(index)
+    |> find_larger()
     new_input = input |> Enum.filter(&(Enum.at(&1, index) == check_val))
-    calc_oxy(new_input, size, val <> check_val, index + 1)
+    calc_oxy(new_input, val <> check_val, index + 1)
   end
 
-  def calc_co2([val], _size, _index), do: Enum.join(val)
+  def calc_co2([val], _index), do: Enum.join(val)
 
-  def calc_co2(input, size, index) do
+  def calc_co2(input, index) do
 
     check_val =
       input
-      |> find_frequencies(size)
+      |> find_frequencies()
       |> Enum.at(index)
       |> find_smaller()
 
     new_input = input |> Enum.filter(&(Enum.at(&1, index) == check_val))
-    calc_co2(new_input, size, index + 1)
+    calc_co2(new_input, index + 1)
   end
 end
