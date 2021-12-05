428bda4571a3812c42ea69d3c0ab16b96aae9343
Akash Khan <akash.khan@aktsk.com>
2021-12-05 22:08:46 +0800

    refactor: Clean up day5 solution

--- lib/aoc/year2021/day5.ex
+++ lib/aoc/year2021/day5.ex
@@ -14,20 +14,26 @@ defmodule AOC.Year2021.Day5 do
     |> calculate_score()
   end
 
-  def calculate_score(list) do
+  def parse_input(list \\ input()) do
+    list
+    |> Enum.map(&String.split(&1, " -> ", trim: true))
+    |> Enum.map(&to_coords/1)
+  end
+
+  defp calculate_score(list) do
     list
     |> Enum.frequencies()
     |> Enum.filter(fn {{_, _}, val} -> val >= 2 end)
     |> length
   end
 
-  def same_xy?([[x1, _], [x1, _]]), do: true
-  def same_xy?([[_, y1], [_, y1]]), do: true
-  def same_xy?(_), do: false
+  defp same_xy?([[x1, _], [x1, _]]), do: true
+  defp same_xy?([[_, y1], [_, y1]]), do: true
+  defp same_xy?(_), do: false
 
-  def check_intersections([], intersections), do: intersections
+  defp check_intersections([], intersections), do: intersections
 
-  def check_intersections([[[x1, y1], [x1, y2]] | tail], intersections) do
+  defp check_intersections([[[x1, y1], [x1, y2]] | tail], intersections) do
     intersections =
       for y <- y1..y2, reduce: intersections do
         acc -> [{x1, y} | acc]
@@ -36,7 +42,7 @@ defmodule AOC.Year2021.Day5 do
     check_intersections(tail, intersections)
   end
 
-  def check_intersections([[[x1, y1], [x2, y1]] | tail], intersections) do
+  defp check_intersections([[[x1, y1], [x2, y1]] | tail], intersections) do
     intersections =
       for x <- x1..x2, reduce: intersections do
         acc -> [{x, y1} | acc]
@@ -45,27 +51,18 @@ defmodule AOC.Year2021.Day5 do
     check_intersections(tail, intersections)
   end
 
-  def check_intersections([[[x1, y1], [x2, y2]] | tail], intersections) do
+  defp check_intersections([[[x1, y1], [x2, y2]] | tail], intersections) do
     check_intersections(tail, intersections ++ Enum.zip(x1..x2, y1..y2))
   end
 
-  def parse_input(list \\ input()) do
-    list
-    |> Enum.map(&String.split(&1, " -> ", trim: true))
-    |> Enum.map(&to_coords/1)
+  defp to_coords(points) do
+    points
+    |> Enum.map(&parse_coords/1)
   end
 
-  defp to_coords([p1, p2]) do
-    [x1, y1] =
-      p1
-      |> String.split(",", trim: true)
-      |> Enum.map(&String.to_integer/1)
-
-    [x2, y2] =
-      p2
-      |> String.split(",", trim: true)
-      |> Enum.map(&String.to_integer/1)
-
-    [[x1, y1], [x2, y2]]
+  defp parse_coords(point) do
+    point
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 end
