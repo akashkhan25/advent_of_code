f0881abdf549fccf1d98e8ad7b23275f1160b766
Akash Khan <akash.khan@aktsk.com>
2021-12-20 18:38:04 +0800

    lib: Add nx

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AOC.MixProject do
     [
       app: :aoc,
       version: "0.1.0",
-      elixir: "~> 1.13-rc",
+      elixir: "~> 1.13.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +21,9 @@ defmodule AOC.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:benchee, "~> 1.0"},
+      {:priority_queue, "~> 1.0"},
+      {:nx, github: "elixir-nx/nx", sparse: "nx"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
