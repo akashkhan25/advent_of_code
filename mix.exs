7fa71f0cb06f42327c4e83a115804a52c2c291b6
Akash Khan <akash.khan@aktsk.com>
2022-12-01 14:02:39 +0800

    lib: Update elixir/erlang versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AOC.MixProject do
     [
       app: :aoc,
       version: "0.1.0",
-      elixir: "~> 1.13.0",
+      elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
